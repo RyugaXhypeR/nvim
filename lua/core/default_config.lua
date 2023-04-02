15d6dca94ce479e50819955f195fab9a1c2d5d08
RyugaXhypeR <ryugashyper@gmail.com>
2023-04-02 10:04:40 +0530

    Change: default config

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -4,8 +4,6 @@ local M = {}
 
 M.options = {
   nvChad = {
-    update_url = "https://github.com/NvChad/NvChad",
-    update_branch = "main",
     update_url = "https://github.com/RyugaXhypeR/nvim",
     update_branch = "master",
   },
@@ -13,15 +11,12 @@ M.options = {
 
 M.ui = {
   -- hl = highlights
-  hl_add = {},
   hl_add = {
     CocErrorSign = {fg = "#f07178"},
     CocInlayHint =  {fg = "#1f2f43"},
   },
   hl_override = {},
   changed_themes = {},
-  theme_toggle = { "onedark", "one_light" },
-  theme = "tokyonight", -- default theme
   theme = "solarized_dark",
   transparency = false,
 }
