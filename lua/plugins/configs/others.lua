3a5dcb0886785a5ecb073e95a313299ffe376255
RyugaXhypeR <ryugashyper@gmail.com>
2023-05-03 19:21:57 +0530

    Addition: Config for lsp-inlayhints

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -186,4 +186,47 @@ M.packer_init = function()
   }
 end
 
+M.inlay_hints = function()
+  local present, inlay_hints = pcall(require, "lsp-inlayhints")
+
+  if not present then
+    return
+  end
+
+  local inlay_hint_config = {
+    inlay_hints = {
+      parameter_hints = {
+        show = true,
+        prefix = "",
+        separator = ", ",
+        remove_colon_start = true,
+        remove_colon_end = true,
+      },
+      type_hints = {
+        -- type and other hints
+        show = true,
+        prefix = " » ",
+        separator = ", ",
+        remove_colon_start = true,
+        remove_colon_end = true,
+      },
+      only_current_line = false,
+      -- separator between types and parameter hints. Note that type hints are
+      -- shown before parameter
+      labels_separator = "",
+      -- whether to align to the length of the longest line in the file
+      max_len_align = false,
+      -- padding from the left if max_len_align is true
+      max_len_align_padding = 1,
+      -- highlight group
+      highlight = "LspInlayHint",
+      -- virt_text priority
+      priority = 0,
+    },
+    enabled_at_startup = true,
+    debug_mode = false,
+  }
+  inlay_hints.setup(inlay_hint_config)
+end
+
 return M
